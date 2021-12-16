9bd70d61b04b78f8bf766df5a088cb55e3720867
Darren <darrenskywalker@hotmail.com>
2021-12-16 07:54:32 -0700

    First commit for HS assessment project

---
+++ src/main/java/com/example/hsdemo/HsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hsdemo/HsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
